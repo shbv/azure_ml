a5a44a80ebb9277d603f9a94b8f3147515ae9647
Shashank B <38381885+shbv@users.noreply.github.com>
2020-12-27 13:34:38 +0000

    hyperdrive code - 1

--- optimize_using_hyperdrive_automl/train.py
+++ optimize_using_hyperdrive_automl/train.py
@@ -14,13 +14,17 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+### YOUR CODE HERE ###
+from azureml.core.dataset import Dataset
+data_file = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds = Dataset.Tabular.from_delimited_files(data_file)
 
 x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
 
 ### YOUR CODE HERE ###a
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
 
 run = Run.get_context()
 
